52e0179b84857b23d334840e96b952f7204fbe95
Lars <lars.schloemer1@gmail.com>
2017-02-19 17:50:32 +0100

    Change to SDL and basic initializaton of fVulkan Instance

---
+++ VRtoBe/main.cpp
@@ -0,0 +1,305 @@
+/*
+ * Vulkan Windowed Program
+ *
+ * Copyright (C) 2016 Valve Corporation
+ * Copyright (C) 2016 LunarG, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+Vulkan C++ Windowed Project Template
+Create and destroy a Vulkan surface on an SDL window.
+*/
+
+// Enable the WSI extensions
+#if defined(__ANDROID__)
+#define VK_USE_PLATFORM_ANDROID_KHR
+#elif defined(__linux__)
+#define VK_USE_PLATFORM_XLIB_KHR
+#elif defined(_WIN32)
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+
+// Tell SDL not to mess with main()
+#define SDL_MAIN_HANDLED
+
+#include <glm/glm.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+#include <vulkan/vulkan.hpp>
+
+#include <iostream>
+#include <vector>
+
+vk::SurfaceKHR createVulkanSurface(const vk::Instance& instance, SDL_Window* window);
+std::vector<const char*> getAvailableWSIExtensions();
+bool checkValidationLayerSupport();
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_LUNARG_standard_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+VkDebugReportCallbackEXT callback;
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+	VkDebugReportFlagsEXT flags,
+	VkDebugReportObjectTypeEXT objType,
+	uint64_t obj,
+	size_t location,
+	int32_t code,
+	const char* layerPrefix,
+	const char* msg,
+	void* userData) {
+
+	std::cerr << "validation layer: " << msg << std::endl;
+
+	return VK_FALSE;
+}
+VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback) {
+	auto func = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+	if (func != nullptr) {
+		return func(instance, pCreateInfo, pAllocator, pCallback);
+	}
+	else {
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+void setupDebugCallback(VkInstance instance) {
+	if (!enableValidationLayers) return;
+	VkDebugReportCallbackCreateInfoEXT createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+	createInfo.pfnCallback = debugCallback;
+	if (CreateDebugReportCallbackEXT(instance, &createInfo, nullptr, &callback) != VK_SUCCESS) {
+		throw std::runtime_error("failed to set up debug callback!");
+	}
+}
+
+#endif
+
+int main()
+{
+    // Use validation layers if this is a debug build, and use WSI extensions regardless
+    std::vector<const char*> extensions = getAvailableWSIExtensions();
+	if (enableValidationLayers)
+		extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+//    std::vector<const char*> layers;
+//#if defined(_DEBUG)
+//    layers.push_back("VK_LAYER_LUNARG_standard_validation");
+//#endif
+
+    // vk::ApplicationInfo allows the programmer to specifiy some basic information about the
+    // program, which can be useful for layers and tools to provide more debug information.
+    vk::ApplicationInfo appInfo = vk::ApplicationInfo()
+        .setPApplicationName("Vulkan C++ Windowed Program Template")
+        .setApplicationVersion(1)
+        .setPEngineName("LunarG SDK")
+        .setEngineVersion(1)
+        .setApiVersion(VK_API_VERSION_1_0);
+
+    // vk::InstanceCreateInfo is where the programmer specifies the layers and/or extensions that
+    // are needed.
+	vk::InstanceCreateInfo instInfo = vk::InstanceCreateInfo()
+		.setFlags(vk::InstanceCreateFlags())
+		.setPApplicationInfo(&appInfo)
+		.setEnabledExtensionCount(static_cast<uint32_t>(extensions.size()))
+		.setPpEnabledExtensionNames(extensions.data());
+	if (enableValidationLayers)
+	{
+		if (!checkValidationLayerSupport()) {
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
+
+		instInfo.setEnabledLayerCount(static_cast<uint32_t>(validationLayers.size()))
+			.setPpEnabledLayerNames(validationLayers.data());
+	}
+	else
+		instInfo.enabledLayerCount = 0;
+
+    // Create the Vulkan instance.
+    vk::Instance instance;
+    try {
+        instance = vk::createInstance(instInfo);
+    } catch(const std::exception& e) {
+        std::cout << "Could not create a Vulkan instance: " << e.what() << std::endl;
+        return 1;
+    }
+
+#ifndef NDEBUG
+	setupDebugCallback(instance.operator VkInstance());
+#endif // !NDEBUG
+
+	
+    // Create an SDL window that supports Vulkan and OpenGL rendering.
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "Could not initialize SDL." << std::endl;
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow("Vulkan Window", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    if(window == NULL) {
+        std::cout << "Could not create SDL window." << std::endl;
+        return 1;
+    }
+
+    // Create a Vulkan surface for rendering
+    vk::SurfaceKHR surface;
+    try {
+        surface = createVulkanSurface(instance, window);
+    } catch(const std::exception& e) {
+        std::cout << "Failed to create Vulkan surface: " << e.what() << std::endl;
+        instance.destroy();
+        return 1;
+    }
+
+    // This is where most initializtion for a program should be performed
+
+    // Poll for user input.
+    bool stillRunning = true;
+    while(stillRunning) {
+
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+
+            switch(event.type) {
+
+            case SDL_QUIT:
+				stillRunning = false;
+                break;
+			case SDL_KEYUP:
+				if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) stillRunning=false;
+				break;
+            default:
+                // Do nothing.
+                break;
+            }
+        }
+
+        SDL_Delay(10);
+    }
+
+    // Clean up.
+    instance.destroySurfaceKHR(surface);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    instance.destroy();
+
+    return 0;
+}
+
+vk::SurfaceKHR createVulkanSurface(const vk::Instance& instance, SDL_Window* window)
+{
+    SDL_SysWMinfo windowInfo;
+    SDL_VERSION(&windowInfo.version);
+    if(!SDL_GetWindowWMInfo(window, &windowInfo)) {
+        throw std::system_error(std::error_code(), "SDK window manager info is not available.");
+    }
+
+    switch(windowInfo.subsystem) {
+
+#if defined(SDL_VIDEO_DRIVER_ANDROID) && defined(VK_USE_PLATFORM_ANDROID_KHR)
+    case SDL_SYSWM_ANDROID: {
+        vk::AndroidSurfaceCreateInfoKHR surfaceInfo = vk::AndroidSurfaceCreateInfoKHR()
+            .setWindow(windowInfo.info.android.window);
+        return instance.createAndroidSurfaceKHR(surfaceInfo);
+    }
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_MIR) && defined(VK_USE_PLATFORM_MIR_KHR)
+    case SDL_SYSWM_MIR: {
+        vk::MirSurfaceCreateInfoKHR surfaceInfo = vk::MirSurfaceCreateInfoKHR()
+            .setConnection(windowInfo.info.mir.connection)
+            .setMirSurface(windowInfo.info.mir.surface);
+        return instance.createMirSurfaceKHR(surfaceInfo);
+    }
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_WAYLAND) && defined(VK_USE_PLATFORM_WAYLAND_KHR)
+    case SDL_SYSWM_WAYLAND: {
+        vk::WaylandSurfaceCreateInfoKHR surfaceInfo = vk::WaylandSurfaceCreateInfoKHR()
+            .setDisplay(windowInfo.info.wl.display)
+            .setSurface(windowInfo.info.wl.surface);
+        return instance.createWaylandSurfaceKHR(surfaceInfo);
+    }
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_WINDOWS) && defined(VK_USE_PLATFORM_WIN32_KHR)
+    case SDL_SYSWM_WINDOWS: {
+        vk::Win32SurfaceCreateInfoKHR surfaceInfo = vk::Win32SurfaceCreateInfoKHR()
+            .setHinstance(GetModuleHandle(NULL))
+            .setHwnd(windowInfo.info.win.window);
+        return instance.createWin32SurfaceKHR(surfaceInfo);
+    }
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_X11) && defined(VK_USE_PLATFORM_XLIB_KHR)
+    case SDL_SYSWM_X11: {
+        vk::XlibSurfaceCreateInfoKHR surfaceInfo = vk::XlibSurfaceCreateInfoKHR()
+            .setDpy(windowInfo.info.x11.display)
+            .setWindow(windowInfo.info.x11.window);
+        return instance.createXlibSurfaceKHR(surfaceInfo);
+    }
+#endif
+
+    default:
+        throw std::system_error(std::error_code(), "Unsupported window manager is in use.");
+    }
+}
+
+std::vector<const char*> getAvailableWSIExtensions()
+{
+    std::vector<const char*> extensions;
+    extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
+
+#if defined(VK_USE_PLATFORM_ANDROID_KHR)
+    extensions.push_back(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_MIR_KHR)
+    extensions.push_back(VK_KHR_MIR_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
+    extensions.push_back(VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_WIN32_KHR)
+    extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_XLIB_KHR)
+    extensions.push_back(VK_KHR_XLIB_SURFACE_EXTENSION_NAME);
+#endif
+
+    return extensions;
+}
+
+bool checkValidationLayerSupport() {
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName : validationLayers) {
+		bool layerFound = false;
+
+		for (const auto& layerProperties : availableLayers) {
+			if (strcmp(layerName, layerProperties.layerName) == 0) {
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound) {
+			return false;
+		}
+	}
+
+	return true;
+}
